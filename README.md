1b21d272d14cdbaba9a8c711d2a744b2e23ff4d9
masahikomori <masahikomori@outlook.jp>
2017-03-23 15:31:59 +0900

    For now

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nslookup2csv
+
+[![Build status](https://ci.appveyor.com/api/projects/status/76xqkgtimheax97j?svg=true)](https://ci.appveyor.com/project/mamori017/nslookup2csv)
+[![Release](https://img.shields.io/github/release/mamori017/nslookup2csv.svg)](https://github.com/mamori017/nslookup2csv/releases/latest)
+[![License](https://img.shields.io/github/license/mamori017/nslookup2csv.svg)](https://github.com/mamori017/nslookup2csv/blob/master/LICENSE)
+
+## Overview
+nslookup‚ÌŒ‹‰Ê‚ðcsvƒtƒ@ƒCƒ‹‰»‚·‚é
+
+## Licence
+[MIT](https://github.com/mamori017/nslookup2csv/blob/master/LICENSE)
+
+## Author
+[Masahiko Mori](https://github.com/mamori017)
---
+++ nslookup2csv/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("nslookup2csv")>
+<Assembly: AssemblyDescription("nslookup2csv")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("nslookup2csv")>
+<Assembly: AssemblyCopyright("Copyright ©  Masahiko Mori 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("8de4b874-a7dd-456b-8213-c8ebae28ca82")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
